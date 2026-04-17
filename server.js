9cccec54b4a219e8bbb571aeb8ff534d6e299190
Luis Canales <37197855+knals17@users.noreply.github.com>
2026-04-17 16:01:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.post('/extract', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Dembrandt API corriendo en el puerto ${PORT}`));
